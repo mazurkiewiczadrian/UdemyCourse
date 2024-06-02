09bb204186c1b8622bd62f741a07a8bf3b8b9e90
mazurkiewiczadrian <mazurkiewicz.adrian.93@gmail.com>
2024-06-02 20:42:13 +0200

    Pierwszy commit

---
+++ src/test/java/HelloWorldJUnit.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class HelloWorldJUnit {
+
+    @Test
+    public void hello(){
+        System.out.println("Hello");
+    }
+
+    @Test
+    public void hello2(){
+        System.out.println("Hello2");
+    }
+}
